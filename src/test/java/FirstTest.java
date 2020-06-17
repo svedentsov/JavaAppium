994685ff3a751d27505a07fd00968cf79e41636a
Игорь Сведенцов <svedentsov@gmail.com>
2020-06-17 22:19:02 +0500

    Assert assertion error

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -231,6 +231,38 @@ public class FirstTest {
         );
     }
 
+    @Test
+    public void testAmountOfEmptySearch() {
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search Wikipedia' input",
+                5
+        );
+
+        String search_line = "zxcasddasdqweqwe";
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search')]"),
+                search_line,
+                "Cannot find search input",
+                5
+        );
+
+        String search_result_locator = "//*[@resource-id='org.wikipedia:id/search_results_list']/*[@resource-id='org.wikipedia:id/page_list_item_container']";
+        String empty_result_label = "//*[contains(@text, 'No results found')]";
+
+        waitForElementPresent(
+                By.xpath(empty_result_label),
+                "Cannot find empty result label by the request " + search_line,
+                15
+        );
+
+        assertElementNotPresent(
+                By.xpath(search_result_locator),
+                "we are found some results by request " + search_line
+        );
+    }
+
     private WebElement waitForElementPresent(By by, String error_massage, long timeoutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
         wait.withMessage(error_massage + "\n");
@@ -313,5 +345,13 @@ public class FirstTest {
     private int getAmountOfElements(By by) {
         List elements = driver.findElements(by);
         return elements.size();
+    }
+
+    private void assertElementNotPresent(By by, String error_messge) {
+        int amount_of_element = getAmountOfElements(by);
+        if (amount_of_element > 0) {
+            String default_result = "An element '" + by.toString() + "' supposed to be not present";
+            throw new AssertionError(default_result + " " + error_messge);
+        }
     }
 }
