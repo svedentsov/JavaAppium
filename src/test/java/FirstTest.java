7e713b384046de04746b381ea79fe85737253ef9
Игорь Сведенцов <svedentsov@gmail.com>
2020-06-18 22:39:27 +0500

    SearchPageObject

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,5 +1,6 @@
 import lib.CoreTestCase;
 import lib.ui.MainPageObject;
+import lib.ui.SearchPageObject;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -16,22 +17,10 @@ public class FirstTest extends CoreTestCase {
 
     @Test
     public void testSearch() {
-        mainPageObject.waitForElementAndClick(
-                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
-                "Cannot find 'Search Wikipedia' input",
-                5
-        );
-        mainPageObject.waitForElementAndSendKeys(
-                By.xpath("//*[contains(@text, 'Search')]"),
-                "Java",
-                "Cannot find search input",
-                5
-        );
-        mainPageObject.waitForElementPresent(
-                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
-                "Connot find 'Object-oriented programming language' topic searching by 'Java'",
-                15
-        );
+        SearchPageObject searchPageObject = new SearchPageObject(driver);
+        searchPageObject.initSearchInput();
+        searchPageObject.typeSearchLine("Java");
+        searchPageObject.waitForSearchResult("Object-oriented programming language");
     }
 
     @Test
--- src/test/java/lib/CoreTestCase.java
+++ src/test/java/lib/CoreTestCase.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.URL;
 
 public class CoreTestCase extends TestCase {
+
     protected AppiumDriver driver;
     private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
 
