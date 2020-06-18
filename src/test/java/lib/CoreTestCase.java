a6a12abd80b3391f79952b0dbfd493d5a13f7c0a
Игорь Сведенцов <svedentsov@gmail.com>
2020-06-18 21:42:25 +0500

    CoreTestCase

---
+++ src/test/java/lib/CoreTestCase.java
@@ -0,0 +1,34 @@
+package lib;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import junit.framework.TestCase;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class CoreTestCase extends TestCase {
+
+    protected AppiumDriver driver;
+    private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestCase");
+        capabilities.setCapability("platformVersion", "6.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "C:\\projects\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
+        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        driver.quit();
+        super.tearDown();
+    }
+}
